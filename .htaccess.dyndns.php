168991cf124dd86bc775437b7221008c2d1d6106
Bruce Scherzinger <16126098+bascherz@users.noreply.github.com>
2021-08-11 21:54:43 -0400

    Update .htaccess.dyndns.php
    Revised to use gethostbyname instead of exec. Simplified slightly. Prettified (maybe).

--- .htaccess.dyndns.php
+++ .htaccess.dyndns.php
@@ -1,18 +1,15 @@
 <?php
-
 /*
-
   Dynamic DNS / DYNDNS HTACCESS Updater
   ^ 12-2014 - david hunt - holodyn.com
-
   Read $inputFileName file
   Look for lines following the format
     # DYNDNS {hostname} {notes}
   Pass {hostname} to shell `host {hostname}`
   Overwrite following line with new result
-
+  ^ 02-2020 - bruce scherzinger
+  Revised to use gethostbyname. Simplified slightly. Prettified (maybe).
  */
-
 /*
   Options
  */
@@ -21,7 +18,6 @@
   $backupInputFile      = true;
   $onErrorUseOriginal   = true;
   $echoOutput           = false;
-
 /*
   Internal Store
  */
@@ -29,38 +25,41 @@
   $followTask           = null;
   $host_ip              = null;
   $host_dns             = null;
-  $host_result          = null;
-
 /*
   Open, read, parse, write, echo
  */
   if( $fh = fopen($inputFileName, 'r') ){
-    while( isset($fh) && !feof($fh) && $line = fgets($fh) ){
+    while( isset($fh) && !feof($fh) && $line = fgets($fh) )
+    {
       $line = trimLine( $line);
-      if( preg_match('/^\# DYNDNS ([0-9A-Za-z\.\-]+)(\s*|\s.*)$/', $line, $matches) ){
+      $host_ip = null;
+      if( preg_match('/^\# DYNDNS ([0-9A-Za-z\.\-]+)(\s*|\s.*)$/', $line, $matches) )
+      {
         # DYNDNS account.dyndns.org
         $followTask = 'dns_allow';
-        $host_ip = null;
         $host_dns = $matches[1];
-        $host_result = trimLine( shell_exec('host ' . $host_dns) );
-        if( !empty($host_result) && preg_match('/^.* ([\d\.]+)$/', $host_result) ){
-          $host_ip = preg_replace('/^.* ([\d\.]+)$/', '$1', $host_result);
-        }
+        $host_ip = gethostbyname($host_dns);
         $outputRows[] = $line;
       }
-      else if( !empty($followTask) ){
-        switch( $followTask ){
+      else if( !empty($followTask) )
+      {
+        switch( $followTask )
+        {
           case 'dns_allow':
-            if( empty($host_ip) ){
-              if( $onErrorUseOriginal ){
+            if( empty($host_ip) )
+            {
+              if( $onErrorUseOriginal )
+              {
                 $outputRows[] = $line;
               }
-              else {
-                $outputRows[] = '# host lookup failed: ' . $host_dns . ' - ' . $host_result;
+              else
+              {
+                $outputRows[] = '# host lookup failed: ' . $host_dns . ' - ' . $host_ip;
               }
             }
-            else {
-              $outputRows[] = 'Allow from ' . $host_ip; // . ' # ' . $host_result;
+            else
+            {
+              $outputRows[] = 'Allow from ' . $host_ip;
             }
             break;
           case 'default':
@@ -69,30 +68,32 @@
         }
         $followTask = null;
       }
-      else {
+      else
+      {
         $outputRows[] = $line;
       }
     }
     fclose( $fh );
-
-    if( $backupInputFile ){
+    if( $backupInputFile )
+    {
       copy( $inputFileName, $inputFileName.'.bak' );
     }
-
-    if( !empty($outputRows) ){
+    if( !empty($outputRows) )
+    {
       $fw = fopen($outputFileName, 'w');
       fwrite( $fw, implode("\n", $outputRows) );
       fclose( $fw );
     }
-
-    if( !empty($echoOutput) ){
-      echo implode( "\n", $outputRows );
+    if( !empty($echoOutput) )
+    {
+      echo implode( "\n", $outputRows )."\n";
     }
   }
-
-/*
+ /*
   Trim CR from tail
  */
-  function trimLine( $line ){
+  function trimLine( $line )
+  {
     return preg_replace('/[\n\r].*/s','',$line);
   }
+?>
